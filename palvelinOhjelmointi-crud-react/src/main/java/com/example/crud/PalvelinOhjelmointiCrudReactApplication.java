cb6f36f057133362ebfc89c8d062686b39ca299e
Paimen85 <evgeni.baranov@gmail.com>
2023-12-07 20:34:39 +0200

    first commit

---
+++ palvelinOhjelmointi-crud-react/src/main/java/com/example/crud/PalvelinOhjelmointiCrudReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalvelinOhjelmointiCrudReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalvelinOhjelmointiCrudReactApplication.class, args);
+	}
+
+}
